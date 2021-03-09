6d67f18fc8bc48e54c97da481eed228fb1b370e1
Kimur1 <kimur53@gmail.com>
2021-03-09 18:17:51 +0000

    index.html

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,8 @@
 import "../style/index.scss";
 
-/**
- *  EDIT ONLY INSIDE THIS RENDER FUNCTION
+/**EDIT ONLY INSIDE THIS RENDER FUNCTION
  *  This function is called every time the user changes types or changes any input
- * 
+  
     {
         includeCover: true, // if includeCover is true the algorithm should
         background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the url of the image that will used as background for the profile cover
@@ -22,6 +21,7 @@ import "../style/index.scss";
         city: null
     }
  */
+
 function render(variables = {}) {
   console.log("These are the current variables: ", variables); //print on the console
   // here we ask the logical questions to make decisions on how to build the html
@@ -33,14 +33,14 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/alesanchezr"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="https://github.com/alesanchezr"><i class="fa fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/alesanchezr"><i class="fa fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/alesanchezr"><i class="fa fa-instagram"></i></a></li>
+          <h1>${variables.name}</h1>
+          <h2>Web Developer ${variables.role}</h2>
+          <h3>${variables.city},${variable.country}</h3>
+          <ul class=${socialMediaPosition}>
+            <li><a href= ${variables.twitter}><i class="fa fa-twitter"></i></a></li>
+            <li><a href= ${variables.github}><i class="fa fa-github"></i></a></li>
+            <li><a href= ${variables.linkedin}><i class="fa fa-linkedin"></i></a></li>
+            <li><a href=${variables.instagram}><i class="fa fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -70,6 +70,7 @@ window.onload = function() {
     country: null,
     city: null
   };
+
   render(window.variables); //render the card for the first time
 
   document.querySelectorAll(".picker").forEach(function(elm) {
